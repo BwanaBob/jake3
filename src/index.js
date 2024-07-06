346fd0d45981b26e395788a298d4acb8e804db43
BwanaBob <bowanabob@yahoo.com>
2024-07-05 22:26:26 -0500

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,41 +1,43 @@
-const path = require('path');
-const Scheduler = require('./modules/Scheduler');
-const Reddit = require('./modules/Reddit');
-const { channel } = require('diagnostics_channel');
-const Logger = require('./modules/Logger');
-const Discord = require('./modules/Discord');
+const path = require('path')
+const Scheduler = require('./modules/Scheduler')
+const Reddit = require('./modules/Reddit')
+const { channel } = require('diagnostics_channel')
+const Logger = require('./modules/Logger')
+const Discord = require('./modules/Discord')
 
-const { discordToken } = require('./credentials');
-const scheduler = new Scheduler();
-const reddit = new Reddit();
-const logger = new Logger();
-const discord = new Discord(discordToken);
+const { discordToken } = require('./credentials')
+const scheduler = new Scheduler()
+const reddit = new Reddit()
+const logger = new Logger()
+const discord = new Discord(discordToken)
 const broker = require('./utils/message-broker')
 
 scheduler.on('jobScheduled', (name, cronExpression) => {
-  // console.log(`Job "${name}" scheduled with cron expression "${cronExpression}"`);
-  logger.info({emoji: '⏰', columns: ["Scheduler", "Schedule Job", name, cronExpression]});
-});
+   logger.info({
+      emoji: '⏰',
+      columns: ['Scheduler', 'Schedule Job', name, cronExpression],
+   })
+})
 
 scheduler.on('jobCancelled', (name) => {
-  // console.log(`Job "${name}" cancelled`);
-  logger.info({ emoji: '⏰', columns: ['Scheduler', 'Cancel Job', name ]})
-});
+   logger.info({ emoji: '⏰', columns: ['Scheduler', 'Cancel Job', name] })
+})
 
 scheduler.on('jobCompleted', (name, result) => {
-  // logger.info({ emoji: '⏰', columns: ['Scheduler', 'Job Complete', name ]})
-  const discordResult = broker.processDiscordMessage(discord.client, name, result)
-  // console.log(result);
-});
+   const discordResult = broker.processDiscordMessage(
+      discord.client,
+      name,
+      result
+   )
+})
 
-const jobsFolderPath = path.join(__dirname, 'jobs');
-scheduler.loadJobsFromFolder(jobsFolderPath, { reddit, logger });
+const jobsFolderPath = path.join(__dirname, 'jobs')
+scheduler.loadJobsFromFolder(jobsFolderPath, { reddit, logger })
 
-(async () => {
-  try {
-    // console.log("logging in")
-    await discord.login();
-  } catch (error) {
-    console.error('Error logging into Discord:', error);
-  }
-})();
+;(async () => {
+   try {
+      await discord.login()
+   } catch (error) {
+      console.error('Error logging into Discord:', error)
+   }
+})()
