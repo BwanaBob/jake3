8e224bff83aee3c9f9a1f08d7e77a5b765c88e30
BwanaBob <bowanabob@yahoo.com>
2024-07-26 00:21:00 -0500

    tweak bingo regex

--- src/utils/message-broker.js
+++ src/utils/message-broker.js
@@ -839,7 +839,7 @@ module.exports = {
                for (const comment of response.data) {
                   let message = this._getCommentMessage(comment, jobName)
                   // ping bingo mod if bingo is mentioned
-                  const bingoExpression = new RegExp('\\bbingo\\b');
+                  const bingoExpression = new RegExp('\\b(thatsa)?bingo\\b');
                   if (
                      comment.subreddit == 'OnPatrolLive' &&
                      comment.body.match(bingoExpression)
@@ -859,7 +859,7 @@ module.exports = {
                for (const post of response.data) {
                   let message = this._getPostMessage(post, jobName)
                   // ping bingo mod if bingo is mentioned
-                  const bingoExpression = new RegExp('\\bbingo\\b');
+                  const bingoExpression = new RegExp('\\b(thatsa)?bingo\\b');
                   if (
                      post.subreddit == 'OnPatrolLive' &&
                      (post.title.match(bingoExpression) ||
