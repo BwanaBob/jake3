7ee3c285b6c63b80344b684f0357ad9bcdc7e9ab
BwanaBob <bowanabob@yahoo.com>
2024-11-22 19:34:34 -0600

    BlueSky fixes

--- src/jobs/bluesky-post-bingo.js
+++ src/jobs/bluesky-post-bingo.js
@@ -1,13 +1,13 @@
 const scheduler = require('../modules/Scheduler')
 const BlueSky = require('../modules/BlueSky.js')
-const { blueSkyBingoUser, blueSkyBingoPassword } = require('../credentials')
-const bluesky = new BlueSky(blueSkyBingoUser, blueSkyBingoPassword)
+const { blueSkyBingoUsername, blueSkyBingoPassword } = require('../credentials')
+const bluesky = new BlueSky(blueSkyBingoUsername, blueSkyBingoPassword)
 const config = require('../config')
 const { postText, imagePaths } = config.jobs.blueSkyPostBingo
 
 module.exports = () => ({
    name: 'blueSkyPostBingo',
-   cronExpression: '0 27 19 * * FRI,SAT', // pre First Shift bingo post - live
+   cronExpression: '0 59 18 * * FRI,SAT', // pre First Shift bingo post - live
    //   cronExpression: '0 0 12 1 1 *', // noon 1/1 (Park It)
    //   cronExpression: '*/15 * * * * *', // Every 15 seconds (testing)
 
