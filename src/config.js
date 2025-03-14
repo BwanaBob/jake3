edc5ec8dfa3a23ce9f69847957611af0bba1ec63
BwanaBob <bowanabob@yahoo.com>
2025-03-14 12:03:27 -0500

    2025-03-14 post

--- src/config.js
+++ src/config.js
@@ -91,8 +91,8 @@ module.exports = {
    },
    jobs: {
       blueSkyPostBingo:{
-         postText:"🚨 Saturday night and we got each other play bingo with! #OnPatrolLive starts in an hour. Get your bingo cards and play along!\n➡️ www.thatsabingo.com\n#OPLive #OPNation",
-         singleImage: "./resources/bingo-splash-chibi-shop.png",
+         postText:"🚨 Another Friday night is upon us, #OPNation! Bike week may be over, but spring break in Daytona could bring us some excitement. Sticks is also back in studio, promising to take a razor to Dan's face.\nIt all kicks off in one hour, so grab your bingo cards and play along!\n➡️ www.thatsabingo.com\n#OPLive #OnPatrolLive",
+         singleImage: "./resources/bingo-splash-leprechaun.png",
          imagePaths: [
             './resources/bingo-splash-chibi-shop.png',
             './resources/bingo-splash-easter-bunny.png',
