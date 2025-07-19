0b4cb27ddd28623dc2b4d81bcb778cfc04efe4c4
BwanaBob <bowanabob@yahoo.com>
2025-07-19 16:11:17 -0500

    park lineup job

--- src/jobs/rss-twitter-lineup.js
+++ src/jobs/rss-twitter-lineup.js
@@ -9,7 +9,8 @@ const FEED_URL = 'https://rss.xcancel.com/danabrams/rss'
 module.exports = () => ({
    name: 'rssTwitterLineup',
    // cronExpression: '0 * * * * *', // Every 60 seconds (testing)
-   cronExpression: '*/2 16-18 * * 5,6', // Every 2 minutes, Friday & Saturday, 4-6pm
+   // cronExpression: '*/2 16-18 * * 5,6', // Every 2 minutes, Friday & Saturday, 4-6pm
+   cronExpression: '0 0 12 11 1 *', // noon 1/1 (Park It)
    jobFunction: async () => {
       if (!module.exports._watcher) {
          module.exports._watcher = new RssWatcher({
